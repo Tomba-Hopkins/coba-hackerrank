83bd0ffe6cc8266fc7fe3d9a9a32870d9aa99754
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-02-21 09:34:49 +0700

    huhu hahahah

---
+++ algorithm/countApplesAndOranges.go
@@ -0,0 +1,19 @@
+package algorithm
+
+// Question :
+
+// Sample Input 0
+// 7 11
+// 5 15
+// 3 2
+// -2 2 1
+// 5 -6
+// Sample Output 0
+// 1
+// 1
+
+// Answer :
+
+func countApplesAndOranges(s int32, t int32, a int32, b int32, apples []int32, oranges []int32) {
+
+}
